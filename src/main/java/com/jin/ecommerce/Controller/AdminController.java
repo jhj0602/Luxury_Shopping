3c5cd7a8dd532bf678302d93c00fbfc1d52c945e
jhj960918 <jhj960918@gmail.com>
2021-01-04 13:59:50 +0900

    관리자 페이지 작성

---
+++ src/main/java/com/jin/ecommerce/Controller/AdminController.java
@@ -0,0 +1,26 @@
+package com.jin.ecommerce.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/category")
+    public String viewCategory(Model model) {
+        // View attribute
+        model.addAttribute("template", "admin/category");
+        return "admin/index";
+    }
+
+    @GetMapping("/users")
+    public String viewAccount(Model model) {
+        // View attribute
+        model.addAttribute("template", "admin/users");
+        return "admin/index";
+    }
+}
